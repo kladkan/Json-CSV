d65ea375b7c38c6c69e547cf7ad5b25af2c8d32d
Aleksey <kladkan@yandex.ru>
2018-10-17 22:32:18 +0300

    Update money.php

--- money.php
+++ money.php
@@ -3,17 +3,27 @@
 //var_dump($argv);
 //$data = $argv;
 $data = ['money.php', '256.00', 'prazdnik', 'keks', 'ttttt'];
+/*$data = ['money.php', '--today'];
+if ($data[1] == '--today') {
+  //открыть из файла значения в виде массива
+  $data = file("./data.csv", FILE_IGNORE_NEW_LINES);
+  print_r($data);
+  exit;
+} else {
+  echo 'Ошибка!';
+}
+*/
 print_r($data);
 $data2words = array_slice($data , 2); // массив: prazdnik keks
-print_r($data2words);
+//print_r($data2words);
 $data2words_string = implode(" ", $data2words); // получаем строку prazdnik keks
-print_r($data2words_string);
+//print_r($data2words_string);
 $data = array_slice($data, 1); //удаляем из первоначального массива название файла
-print_r($data);// получаем массив '256.00', 'prazdnik', 'keks'
+//print_r($data);// получаем массив '256.00', 'prazdnik', 'keks'
 array_splice($data, 1);//удаляем из массива все начиная со второй ячейки
-print_r($data);//получаем 256.00
+//print_r($data);//получаем 256.00
 array_unshift($data, date('Y-m-d').",");
-print_r($data);
+//print_r($data);
 $data[] = ','."$data2words_string\n";
 print_r($data);
 
@@ -24,6 +34,7 @@ $resource = fopen($filedata, "a+");
 file_put_contents($filedata, $data, FILE_APPEND | LOCK_EX);
 $getdata = file("./data.csv", FILE_IGNORE_NEW_LINES);
 print_r($getdata);
+echo 'Добавлена строка:';
 
 /*черновик
 
