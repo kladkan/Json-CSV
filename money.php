59e044e57d5ee2abc67a8168b1fcf892fb633e63
Aleksey <kladkan@yandex.ru>
2018-10-17 20:50:09 +0300

    Update money.php

--- money.php
+++ money.php
@@ -6,15 +6,13 @@ $data = ['money.php', '256.00', 'prazdnik', 'keks'];
 print_r($data);
 $data2words = array_slice($data , 2); // массив: prazdnik keks
 print_r($data2words);
-$data2words_string = implode(",", $data2words); // строка prazdnik,keks
+$data2words_string = implode(" ", $data2words); // получаем строку prazdnik keks
 print_r($data2words_string);
-$data2words_string = str_replace (',', ' ', $data2words_string);
-echo "\n $data2words_string";
 $data = array_slice($data, 1); //удаляем из первоначального массива название файла
 print_r($data);// получаем массив '256.00', 'prazdnik', 'keks'
 array_splice($data, 1);//удаляем из массива все начиная со второй ячейки
 print_r($data);//получаем 256.00
-array_unshift($data, date('Y-m-d'));
+array_unshift($data, "date('Y-m-d'),");
 print_r($data);
 $data[] = "$data2words_string\n";
 print_r($data);
