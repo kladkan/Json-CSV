7859672ebec150c36126b26d8189cf48299b6d21
Aleksey <kladkan@yandex.ru>
2018-10-19 17:10:51 +0300

    Update visa.php

--- visa.php
+++ visa.php
@@ -1,4 +1,19 @@
 <?php
-	$data = file_get_contents('https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv');
-	print_r($data);
+	$country = $argv;
+	$file = fopen("https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv", "r");
+		if ($file == true) {
+			for ($i = 1; $i > 0; $i++) {
+				$array_row = fgetcsv($file, 1000, ",");
+				if ($array_row == true) {
+					if ($array_row[1] == $country[1]) {
+						echo $country[1].': '.$array_row[4];
+						fclose($file);
+						exit;
+					}
+				}
+			}
+		}else {
+			echo 'Источник недоступен!';
+		}
+
 ?>
