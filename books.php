2326465b31afdd1fdd512b7c33310a25095b70a0
Aleksey <kladkan@yandex.ru>
2018-10-19 23:54:52 +0300

    Update books.php

--- books.php
+++ books.php
@@ -3,10 +3,20 @@ $data = $argv;
 $book = array_slice($data , 1);
 $book = implode(" ", $book);
 $encode = urlencode($book);
-$url = 'https://www.googleapis.com/books/v1/volumes?q={'.$encode.'}';
-$response = file_get_contents("$url");
+$url = 'https://www.gogleapis.com/books/v1/volumes?q={'.$encode.'}';
+$response = @file_get_contents("$url");
+if ($response === false) {
+    $response = file_get_contents("./books.json");
+    $content = json_decode($response, true)["content"];
+} else {
+    $cache = [
+        "date" => date("d.m.Y"),
+        "content" => $content
+    ];
+    file_put_contents("./books.json", json_encode($cache));
+}
 $content = json_decode($response, true);
-//print_r($content);
-file_put_contents("./books.json", $response);
+print_r($content);
+
 
 ?>
