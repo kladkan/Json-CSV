ae9af94d8697c7c8528b1b5c92fc8f35818168f8
Aleksey <kladkan@yandex.ru>
2018-10-21 22:33:00 +0300

    Update books.php

--- books.php
+++ books.php
@@ -1,22 +1,56 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('errors_reporting', E_ALL);
 $data = $argv;
 $book = array_slice($data , 1);
 $book = implode(" ", $book);
 $encode = urlencode($book);
-$url = 'https://www.gogleapis.com/books/v1/volumes?q={'.$encode.'}';
-$response = @file_get_contents("$url");
-if ($response === false) {
+$url = 'https://www.googleapis.com/books/v1/volumes?q={'.$encode.'}';
+$response = file_get_contents("$url");
+if ($response === false) { // проверить на ошибки загрузки ФУНКЦИЯ JSON_LAST_ERROR
     $response = file_get_contents("./books.json");
-    $content = json_decode($response, true)["content"];
-} else {
-    $cache = [
-        "date" => date("d.m.Y"),
-        "content" => $content
-    ];
-    file_put_contents("./books.json", json_encode($cache));
 }
 $content = json_decode($response, true);
-print_r($content);
+
+
+foreach ($content as $key => $value) {
+    if (is_array($value)) {
+        //echo $key;
+        foreach ($value as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $key => $value) {                    
+                    switch ($key) {
+                        case "id":
+                            echo $value.', ';
+                            break;
+                        case "volumeInfo":
+                            foreach ($value as $key => $value) {
+                                switch ($key) {
+                                    case "title":
+                                        echo $value.', ';
+                                        break;
+                                    case "authors":
+                                        $authors = implode(",", $value);
+                                        echo $authors."\n";
+                                        break;
+                                    
+                                    default:
+                                        # code...
+                                        break;
+                                }
+                            }
+                            break;
+                        default:
+                            # code...
+                            break;
+                    } 
+                }
+            }
+        }
+        
+    }
+}
+
 
 
 ?>
