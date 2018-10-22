bcba63ade5744bc253d04955e5f34f72a442047c
Aleksey <kladkan@yandex.ru>
2018-10-22 18:53:23 +0300

    Update visa.php

--- visa.php
+++ visa.php
@@ -7,7 +7,7 @@
 			array_splice($country, 1);
 			$country [] = $longname;
 		}
-		$file = fopen("https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8", "r");
+		$file = @fopen("https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8", "r");
 		if ($file === false) {
 			$file = fopen("https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv", "r");
 		}
@@ -16,19 +16,19 @@
 				$array_row = fgetcsv($file, 1000, ",");
 				if ($array_row == true) {
 					if ($array_row[1] == $country[1]) {
-						echo $country[1].': '.$array_row[4];
+						echo "\n".'Страна: '.$country[1].'. Режим въезда с общегражданским паспортом: '.$array_row[4].'.'."\n";
 						fclose($file);
 						exit;
 					}
 				} else {
-					echo 'Страна не найдена или допущена ошибка в названии';
+					echo "\n".'Страна не найдена или допущена ошибка в названии.'."\n";
 					exit;
 				}
 			}
 		} else {
-			echo 'Источник недоступен! Обратитесь позднее';
+			echo "\n".'Источник недоступен! Обратитесь позднее.'."\n";
 		}
 	} else {
-		echo "\n".'Ошибка! Аругмент скрипта не задан. Введите название страныю'."\n";
+		echo "\n".'Ошибка! Аругмент скрипта не задан. Введите название страны.'."\n";
 	}
 ?>
