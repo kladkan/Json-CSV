8030e5ad3c69a76e6356a371c5fe76092b19562a
Aleksey <kladkan@yandex.ru>
2018-10-24 22:11:32 +0300

    Update visa.php

--- visa.php
+++ visa.php
@@ -11,24 +11,30 @@
 		if ($file === false) {
 			$file = fopen("https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv", "r");
 		}
+
+		if (fgetcsv($file, 1000, ",") == false && fgetcsv($file, 1000, ",") == null) {
+			echo 'Файл-источник имеет неправильный формат';
+			exit;
+		}
+
 		if ($file == true) {
 			for ($i = 1; $i > 0; $i++) {
 				$array_row = fgetcsv($file, 1000, ",");
 				if ($array_row == true) {
 					if ($array_row[1] == $country[1]) {
-						echo "\n".'Страна: '.$country[1].'. Режим въезда с общегражданским паспортом: '.$array_row[4].'.'."\n";
+						echo "\nСтрана: {$country[1]}. Режим въезда с общегражданским паспортом: {$array_row[4]}.\n";
 						fclose($file);
 						exit;
 					}
 				} else {
-					echo "\n".'Страна не найдена или допущена ошибка в названии.'."\n";
+					echo "\nСтрана не найдена или допущена ошибка в названии.\n";
 					exit;
 				}
 			}
 		} else {
-			echo "\n".'Источник недоступен! Обратитесь позднее.'."\n";
+			echo "\nИсточник недоступен! Обратитесь позднее.\n";
 		}
 	} else {
-		echo "\n".'Ошибка! Аругмент скрипта не задан. Введите название страны.'."\n";
+		echo "\nОшибка! Аругмент скрипта не задан. Введите название страны.\n";
 	}
 ?>
